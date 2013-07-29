27a4913320194658cbd1b2fddf95ebdde19fc91f
Mygnu <harman@mygnu.org>
2013-07-29 21:07:17 +0930

    another commit

--- drunkPirate.py
+++ drunkPirate.py
@@ -4,3 +4,16 @@ A social science student records the angle of each turn before the next 100 step
 Her experimental data is [160, -43, 270, -97, -43, 200, -940, 17, -86]. 
 (Positive angles are counter-clockwise.) 
 Use a turtle to draw the path taken by our drunk friend.'''
+
+
+import turtle
+
+pirate = turtle.Turtle()
+
+pirate.color('green')
+pirate.pensize(5)
+angles = [160,-43,270,-97,-43,200,-940,17,-86]
+
+for i in angles:
+    pirate.left(i)
+    pirate.forward(100)
