092648441d0c42404c0fbae7b4b8d098eb6ca798
Mygnu <harman@mygnu.org>
2013-07-29 22:26:30 +0930

    created

--- drunkPirate.py
+++ drunkPirate.py
@@ -8,12 +8,17 @@ Use a turtle to draw the path taken by our drunk friend.'''
 
 import turtle
 
-pirate = turtle.Turtle()
-
+pirate = turtle.Turtle() #creates the turtle object
 pirate.color('green')
 pirate.pensize(5)
-angles = [160,-43,270,-97,-43,200,-940,17,-86]
 
-for i in angles:
-    pirate.left(i)
+angles = [160,-43,270,-97,-43,200,-940,17,-86] # creating a list of collected data
+direction = 0 
+
+for i in angles:      # iterate through the list
+    pirate.left(i)    # turn left because positive values are anticlockwise
     pirate.forward(100)
+    direction = i + direction # add up all the angles while looping through the list
+
+print('the pirate is going in ',abs((direction // 360)),'degrees right form the original direction')
+
