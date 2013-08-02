85e48a94c611037ade766883bf7c4cb8e41332c8
Mygnu <harman@mygnu.org>
2013-08-02 15:02:42 +0930

    commnts added

--- creatingACircle.py
+++ creatingACircle.py
@@ -8,23 +8,22 @@ tcircle = turtle.Turtle()
 def mycircle(t,r):
     '''Creates circle by taking two arguments "turtle object" and "diameter".'''
 
-    cirf = 2 * r * pi
-    fwd = cirf/100
+    cirf = 2 * r * pi # calculating the circumference of the circle
+    fwd = cirf/100    # to forward the turtle 100 times on the circumference 
     for i in range(100):
         t.forward(fwd)
-        t.right(3.6)
+        t.right(3.6) # turn right 100 times to complete the circle 360 / 100
 
 def cofc(t,r):
     '''Creates circle of circle by taking two arguments
     "turtle object" and "diameter".'''
-    coc_cirf = 2 * r * pi
-    fwd = coc_cirf / 50
-    r_of_small_c = fwd / 2
-    t.speed(10)
+    coc_cirf = 2 * r * pi  # calculating the circumference of the circle
+    fwd = coc_cirf / 50    # forward by the diameter of small circles
+    r_of_small_c = fwd / 2 # radious of small circles
+    t.speed(0)
     for i in range(100):
         mycircle(t,r_of_small_c)     
         t.up()
-        
         t.right(3.6)
         t.forward(fwd)
         t.down()
